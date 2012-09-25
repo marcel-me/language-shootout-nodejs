d9e5591a8da8af4ac804400f6072b7a0105d9769
marcel-me <m.medak@web.de>
2012-09-25 22:16:57 +0200

    implemented csv parser

--- app.js
+++ app.js
@@ -3,15 +3,36 @@ var fs = require('fs');
 console.log('Language Shootout with node.js');
 
 var csvParser = {
-  process: function(fileName) {
-    var stream = fs.createReadStream(fileName, { bufferSize: 64 });
+  read: function(fileName, callback) {
+    var stream = fs.createReadStream(fileName, { encoding: 'utf8' });
+    var buffer = "", data = [];
+    
+    //buffer stream
     stream.addListener('data', function(data) {
-      console.log("Received:\n");
-      console.log(data.toString());
-      console.log("\n");
+      buffer += data;
     });
+
+    //fill data
+    stream.addListener('end', function() {
+      var lines = buffer.split('\n'), header;
+      for(var i = 0; i < lines.length; i++) {
+        if(i == 0) {
+          //read header
+          header = lines[0].split(";")
+        } else {
+          //read data 
+          var values = lines[1].split(";"), record = {};
+          for(var k = 0; k < values.length; k ++) {
+            record[header[k]] = values[k];
+          }
+          data.push(record);
+        }
+      }
+      callback(data);
+    })
   }
 }
 
-csvParser.process('data/song_db_unicode.csv');
-console.log("finished");
+csvParser.read('data/song_db_unicode.csv', function(data) {
+  console.log(data.length);
+});
